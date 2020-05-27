027ea442cef3d888f602da8f23d05890a2f2521d
levoskaa <laurinyecz.levi@gmail.com>
2020-05-27 10:56:41 +0200

    Create Splash Screen
    + transition animation

---
+++ app/src/main/java/hu/bme/aut/mydiary/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package hu.bme.aut.mydiary
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var tv: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    tv = findViewById(R.id.tv)
+                    val animation = AnimationUtils.loadAnimation(baseContext, R.anim.splash_transition)
+                    tv.startAnimation(animation)
+                    Thread.sleep(1500)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
